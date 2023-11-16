064535b8bed6fc04fe85b595e54b7adfa1a08dd0
Christian Bergschneider <magiclake@gmx.de>
2023-11-16 18:14:56 +0100

    fix: switch arduino firmware to new standard

--- src/arduino/firmware/ftSwarmPwrDrive/ftSwarmPwrDrive.ino
+++ src/arduino/firmware/ftSwarmPwrDrive/ftSwarmPwrDrive.ino
@@ -17,15 +17,12 @@
     PSRAM: "enabled"
 
 */
- 
+
 #include "ftSwarmPwrDrive.h"
 
 void setup() {
 
   Serial.begin(115200);
-
-  ftSwarm.verbose(true);
-  FtSwarmSerialNumber_t local = ftSwarm.begin( false );
 
   firmware();
   ESP.restart();
--- src/arduino/firmware/ftSwarmRS/ftSwarmRS.ino
+++ src/arduino/firmware/ftSwarmRS/ftSwarmRS.ino
@@ -17,15 +17,12 @@
     PSRAM: "enabled"
 
 */
- 
+
 #include "ftSwarmRS.h"
 
 void setup() {
 
   Serial.begin(115200);
-
-  ftSwarm.verbose(true);
-  FtSwarmSerialNumber_t local = ftSwarm.begin( false );
 
   firmware();
   ESP.restart();
